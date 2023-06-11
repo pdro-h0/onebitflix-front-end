f034e338a52296bb1b896b9b4b5f6e333129bad7
pdro-h0 <pedro.pho792@gmail.com>
2023-06-11 16:01:23 -0300

    Configuração incial do projeto, configurando arquivos e pastas. Instalação do sass e do bootstrap.

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from '../styles/HomeNotAuth.module.scss'
+
+export const HomeNotAuth = () => {
+  return (
+    <>
+      <Head>
+        <h1>Titulo</h1>
+        <main></main>
+      </Head>
+    </>
+  );
+};
