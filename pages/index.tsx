10794047c76308f607d872560865a67feb63b138
pdro-h0 <pedro.pho792@gmail.com>
2023-06-11 17:43:18 -0300

    Configurando pastas e componentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import Head from "next/head";
-import styles from '../styles/HomeNotAuth.module.scss'
+import styles from "../styles/HomeNotAuth.module.scss";
 
-export const HomeNotAuth = () => {
+const HomeNotAuth = () => {
   return (
     <>
       <Head>
-        <h1>Titulo</h1>
-        <main></main>
+        <title>OneBitFlix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
       </Head>
+      <main>
+        <h1>TESTE</h1>
+      </main>
     </>
   );
 };
+
+export default HomeNotAuth;
