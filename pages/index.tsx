bd3ad78f5c5b4633df8c9e8ef080e4a8d72905a0
pdro-h0 <pedro.pho792@gmail.com>
2023-06-12 12:27:02 -0300

    Criação dos elementos, do visual e também da sua responsividade - Seção de cards, homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
-import { HeaderNoAuth } from "@/components/HeaderNotAuth";
-import { PresentationSection } from "@/components/PresentationSection";
+import { HeaderNoAuth } from "@/components/HomeNoAuth/HeaderNoAuth";
+import { PresentationSection } from "@/components/HomeNoAuth/PresentationSection";
+import { CardsSection } from "@/components/HomeNoAuth/CardsSection";
 
 const HomeNoAuth = () => {
   return (
@@ -10,13 +11,17 @@ const HomeNoAuth = () => {
         <title>OneBitFlix</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
         <meta property="og:title" content="Onebitflix" key="title" />
-				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."
+        ></meta>
       </Head>
       <main>
         <div className={styles.sectionBackground}>
           <HeaderNoAuth />
           <PresentationSection />
         </div>
+        <CardsSection />
       </main>
     </>
   );
